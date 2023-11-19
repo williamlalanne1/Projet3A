fc0e6d72532d3d521e324d9c138aa826238c2645
williamlalanne1 <william.lalanne@centrale-marseille.fr>
2023-11-19 23:21:06 +0100

    ajout de la page réinitialisation du mot de passe

---
+++ Page_ChangementMdp/mdp.js
@@ -0,0 +1,17 @@
+const menuLogo = document.getElementById("menuLogo");
+const popupMenu = document.getElementById("popupMenu");
+var isOpen = false;
+
+menuLogo.addEventListener("click", () => {
+    if (!isOpen) {
+        console.log(isOpen);
+        popupMenu.style.visibility='visible';
+        isOpen = true;
+    }
+    else {
+        console.log(isOpen);
+        popupMenu.style.visibility='hidden';
+        isOpen = false;
+    }
+
+});
